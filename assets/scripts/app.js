eba03f2106bbe8ec80639dcc4745b073f5d1f228
jaimielogan <jaimielogan@gmail.com>
2016-07-22 15:42:16 -0600

    Initial Commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,31 @@
 // Your code here!
+$(document).ready(function(){
+  $.ajax({
+      url: 'http://galvanize-student-apis.herokuapp.com/gpersonnel/roles',
+      error: function(err) {console.error(err);},
+      method: 'GET',
+      success: function(data) {
+        $('.roles').change(function() {
+          var userRole = $('.roles :selected').text();
+          for (var i = 0; i < data.length; i++) {
+            if (data[i].title === userRole) {
+              // Update Image
+              $(".image").attr("src", data[i].img);
+            }
+          }
+        });
+
+        // Submit Button
+        $(".save").on("submit",function(event){
+          event.preventDefault();
+          $.ajax({
+            type: "POST",
+            url: "http://galvanize-student-apis.herokuapp.com/gpersonnel/users",
+            data: { firstName: $("#first").val(), lastName: $("#last").val(), role: userRole },
+            success: $(".save-status").text("Success!").fadeIn(2000).fadeOut(2000)
+          });
+        });
+
+      }
+    });
+  });
