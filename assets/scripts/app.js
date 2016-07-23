b8abe0581d64aeefbe78156aa0b007554477e5cd
jaimielogan <jaimielogan@gmail.com>
2016-07-23 13:17:08 -0600

    Updated to load dropdown menu from API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
       error: function(err) {console.error(err);},
       method: 'GET',
       success: function(data) {
+
+// Working on dynamically loading in the data from the API
+        for(var i of data){
+         var $newOpt = $("<option>");
+         $newOpt = $newOpt.text(i.title);
+         $newOpt = $newOpt.attr("val", i.title);
+         $(".roles").append($newOpt);
+       }
+
         $('.roles').change(function() {
           var userRole = $('.roles :selected').text();
           for (var i = 0; i < data.length; i++) {
@@ -16,15 +25,23 @@ $(document).ready(function(){
         });
 
         // Submit Button
-        $(".save").on("submit",function(event){
-          event.preventDefault();
+        $(".save").submit(function(event){
+          // event.preventDefault();
+          // $(".save-status").text("Success!").fadeIn(2000).fadeOut(2000);
           $.ajax({
             type: "POST",
             url: "http://galvanize-student-apis.herokuapp.com/gpersonnel/users",
             data: { firstName: $("#first").val(), lastName: $("#last").val(), role: userRole },
-            success: $(".save-status").text("Success!").fadeIn(2000).fadeOut(2000)
+            success: function(data){
+              alert(data);
+              $(".save-status").text("Success!").fadeIn(2000).fadeOut(2000);
+            }
+
           });
+          event.preventDefault();
+          $(".save-status").text("Success!").fadeIn(2000).fadeOut(2000);
         });
+
 
       }
     });
